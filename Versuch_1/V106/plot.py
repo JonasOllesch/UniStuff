86eecbff75191f7fc18f9fc63b2c696f27d8fff9
Lukas Annuss <lukas.annuss@tu-dortmund.de>
2021-11-19 18:35:34 +0100

    weiter

--- Versuch_1/V106/plot.py
+++ Versuch_1/V106/plot.py
@@ -2,7 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 from uncertainties import ufloat
 
-#freie Pendel sozusagen alls null Messung
+# do list
+# w+
+# w-
+# kopplungskonstante k                                      (Haken)
+# Schwebungsdauer                                            (Haken)
+#Schwebungsdauer mit den gemessenen verleichen
+#Fehlerfortpflanzung für irgendwie alles xD
 
 #erstesfreiesPendel = np.genfromtxt('l070T15f.txt', unpack =True) #x Zahl der Messung, erstesfreiesPendel Periodendauer x5 des Pendels 
 #erstesfreiesPendel *= (1/5) #Werte duruch 5 teilen
@@ -30,7 +36,7 @@ for j in range(0,2):
 
     for i in range(0, 6):
         Standardabweichung[j][i]= np.std(Werte[j][i])
-print(Standardabweichung)
+#print(Standardabweichung)
 
 StandardabweichungMittelwert = [[1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6]]
 for j in range(0,2):
@@ -38,10 +44,28 @@ for j in range(0,2):
     for i in range(0, 6):
 
         StandardabweichungMittelwert[j][i] = (Standardabweichung[j][i])/np.sqrt(10)
-print(StandardabweichungMittelwert)
+#print(StandardabweichungMittelwert)
+#Kopplungskonstante aus Tp und Tm vom Pendel und langen Pendel berechnen 
+k = (Mittelwerte[0][2]**2 - Mittelwerte[0][3]**2) / (Mittelwerte[0][2]**2 + Mittelwerte[0][3]**2)
+print(k, " k")
+langk = (Mittelwerte[1][2]**2 - Mittelwerte[1][3]**2) / (Mittelwerte[1][2]**2 + Mittelwerte[1][3]**2)
+print(langk, " langk")
 
+#Schwebungsdauer Ts aus Tp und Tm berechnen
+Ts = (Mittelwerte[0][2]* Mittelwerte[0][3]) / (Mittelwerte[0][2]- Mittelwerte[0][3])
+print(Ts, " Ts")
+langTs =Ts = (Mittelwerte[1][2]* Mittelwerte[1][3]) / (Mittelwerte[1][2]- Mittelwerte[1][3])
+print(Ts, " langTs")
 
-
+# in der Form gemessen wp wm ws theoretische KREIS-Frequenzen wp wm ws
+# in der zweiten Spalte das gleich für das lange Pendel 
+Schwingungsfrequenzen = [[1, 2, 3, 4, 5, 6], [1, 2, 3, 4, 5, 6]]
+for i in range (0,2):
+    for j in range(0,2):
+          Schwingungsfrequenzen[j][i] = 1/Mittelwerte[j][i]
+for j in range(0,2):
+    Schwingungsfrequenzen[j][2] = 1/Mittelwerte[j][5]
+print(Schwingungsfrequenzen)
 
 
 plt.subplot(1, 2, 1)
