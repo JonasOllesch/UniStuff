f2c6e74c1ca4527b415664c8e325249b48670ad1
Lukas Annuss <lukas.annuss@tu-dortmund.de>
2021-11-23 21:31:23 +0100

    Vorlage fur plots

--- Versuch_2/V206/plot.py
+++ Versuch_2/V206/plot.py
@@ -5,6 +5,8 @@ import scipy.constants as const
 import sympy
 from scipy.stats import linregress
 import uncertainties.unumpy as unp
+from pandas import read_csv
+from scipy.optimize import curve_fit
 
 #output = ("Werte")    
 #my_file = open(output + '.txt', "w") 
@@ -20,6 +22,42 @@ print(Werte)
 
 
 
+# fit a straight line to the economic data
+from numpy import arange
+from pandas import read_csv
+from scipy.optimize import curve_fit
+from matplotlib import pyplot
+
+# define the true objective function
+def objective(x, a, b, c):
+	return a* (x**2) + b*x + c
+x = Werte[:,0]
+y = Werte[:,1]
+# load the dataset
+# choose the input and output variables
+# curve fit
+popt, _ = curve_fit(objective, x, y)
+# summarize the parameter values
+a, b, c = popt
+print('y = %.5f a * (x ** 2) + %.5f *b + %.5f c' % (a, b,c))
+# plot input vs output
+pyplot.scatter(x, y)
+# define a sequence of inputs between the smallest and largest known inputs
+x_line = arange(min(x), max(x), 0.5)
+print(x_line)
+# calculate the output for the range
+y_line = objective(x_line, a, b,c)
+print(y_line)
+# create a line plot for the mapping function
+pyplot.plot(x_line, y_line, '--', color='red')
+pyplot.show()
+
+
+#print(Werte[0,:])  von links nach rechts
+#print(Werte[1,:]) von links nach rechts
+
+#print(Werte[:,0]) von oben nach unten
+#print(Werte[:,1]) von oben nach unten
 
 
 
