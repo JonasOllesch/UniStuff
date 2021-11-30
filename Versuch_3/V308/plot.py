702aa9e0484084b12d0451d5f63dadc5618337c7
Lukas Annuss <lukas.annuss@tu-dortmund.de>
2021-11-30 16:19:54 +0100

    die werte sind drin

--- Versuch_3/V308/plot.py
+++ Versuch_3/V308/plot.py
@@ -1,21 +1,47 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x = np.linspace(0, 10, 1000)
-y = x ** np.sin(x)
+#output = ("WertelangeSpule15")    
+#my_file = open(output + '.txt', "w") 
+#for i in range(1, 11):
+#    j = i +0.5
+#    my_file.write(str(j))
+#    my_file.write("\n")
+#for i in range (16,24):
+#    my_file.write(str(i))
+#    my_file.write("\n")
+#for i in range(1, 11):
+#    j =10-i
+#    my_file.write(str(j))
+#    my_file.write("\n")
+#for i in range(0, 11):
+#    j =-i
+#    my_file.write(str(j))
+#    my_file.write("\n")
+#for i in range(1, 11):
+#    j =-(10-i)
+#    my_file.write(str(j))
+#    my_file.write("\n")
+#for i in range(1, 11):
+#    my_file.write(str(i))
+#    my_file.write("\n")
 
-plt.subplot(1, 2, 1)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
-plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
-plt.legend(loc='best')
-
-plt.subplot(1, 2, 2)
-plt.plot(x, y, label='Kurve')
-plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
-plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
-plt.legend(loc='best')
-
+#
+#
+#x = np.linspace(0, 10, 1000)
+#y = x ** np.sin(x)
+#
+#plt.subplot(1, 2, 1)
+#plt.plot(x, y, label='Kurve')
+#plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
+#plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
+#plt.legend(loc='best')
+#
+#plt.subplot(1, 2, 2)
+#plt.plot(x, y, label='Kurve')
+#plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
+#plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
+#plt.legend(loc='best')
 # in matplotlibrc leider (noch) nicht möglich
-plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-plt.savefig('build/plot.pdf')
+#plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
+#plt.savefig('build/plot.pdf')
