8a7bd8c6015e05fdf647c58c05e85175e5f87ff1
Lukas Annuss <lukas.annuss@tu-dortmund.de>
2021-12-05 11:49:22 +0100

    die Plots stehen

--- Versuch_3/V308/plot.py
+++ Versuch_3/V308/plot.py
@@ -7,98 +7,78 @@ WerteHysterese[:,1] = WerteHysterese[:,1] / 1000 #von mT in T umrechnen
 x = WerteHysterese[:,0]
 y = WerteHysterese[:,1]
 
-pyplot.scatter(x, y, s=15)
+pyplot.scatter(x, y, color='blue',s=15, label=r'$B$')
 pyplot.legend()
-#pyplot.plot(x, y, '--', color='red')
-#pyplot.show()
-pyplot.savefig('build/Hystereseplot.pdf')
-
-WerteHysterese = np.array(np.genfromtxt('WerteHysterese.txt'))
-WerteHysterese[:,1] = WerteHysterese[:,1] / 1000 #von mT in T umrechnen
+pyplot.grid()
+pyplot.xlabel(r'$I \mathbin{/} \unit{\ampere}$')
+pyplot.ylabel(r'$B \mathbin{/}\unit{\tesla}$')
+pyplot.show()
+pyplot.savefig('build/Hysteresplot.pdf')
 pyplot.clf()
 
-
-
-
-
+#-----------------------------------
 WertelangeSpule = np.array(np.genfromtxt('WertelangeSpule.txt'))
 WertelangeSpule[:,1] = WertelangeSpule[:,1] / 1000 #von mT in T umrechnen
-WertelangeSpule[:,1] = WertelangeSpule[:,1] / 100   # von cm in m umrechnen
+WertelangeSpule[:,0] = WertelangeSpule[:,0] / 100 # von cm in m
 
 x = WertelangeSpule[:,0]
 y = WertelangeSpule[:,1]
 
-pyplot.scatter(x, y, s=15)
+pyplot.scatter(x, y, color='blue',s=15, label=r'$B$')
 pyplot.legend()
-#pyplot.plot(x, y, '--', color='red')
-#pyplot.show()
-pyplot.savefig('build/langeSpule.pdf')
+pyplot.grid()
+pyplot.xlabel(r'$M \mathbin{/} \unit{\meter}$')
+pyplot.ylabel(r'$B \mathbin{/}\unit{\tesla}$')
+pyplot.show()
+pyplot.savefig('build/WertelangeSpule.pdf')
 pyplot.clf()
 
-
-
-
+#-----------------------------------
 WerteSpulenPaar10 = np.array(np.genfromtxt('WerteSpulenPaar10.txt'))
 WerteSpulenPaar10[:,1] = WerteSpulenPaar10[:,1] / 1000 #von mT in T umrechnen
-WerteSpulenPaar10[:,1] = WerteSpulenPaar10[:,1] / 100   # von cm in m umrechnen
+WerteSpulenPaar10[:,0] = WerteSpulenPaar10[:,0] / 100 # von cm in m
 
 x = WerteSpulenPaar10[:,0]
 y = WerteSpulenPaar10[:,1]
 
-pyplot.scatter(x, y, s=15)
+pyplot.scatter(x, y, color='blue',s=15, label=r'$B$')
 pyplot.legend()
-#pyplot.plot(x, y, '--', color='red')
-#pyplot.show()
+pyplot.grid()
+pyplot.xlabel(r'$M \mathbin{/} \unit{\meter}$')
+pyplot.ylabel(r'$B \mathbin{/}\unit{\tesla}$')
+pyplot.show()
 pyplot.savefig('build/WerteSpulenPaar10.pdf')
 pyplot.clf()
-
-
-
+#-----------------------------------
 WerteSpulenPaar20 = np.array(np.genfromtxt('WerteSpulenPaar20.txt'))
 WerteSpulenPaar20[:,1] = WerteSpulenPaar20[:,1] / 1000 #von mT in T umrechnen
-WerteSpulenPaar20[:,1] = WerteSpulenPaar20[:,1] / 100   # von cm in m umrechnen
+WerteSpulenPaar20[:,0] = WerteSpulenPaar20[:,0] / 100 # von cm in m
 
 x = WerteSpulenPaar20[:,0]
 y = WerteSpulenPaar20[:,1]
 
-pyplot.scatter(x, y, s=15)
+pyplot.scatter(x, y, color='blue',s=15, label=r'$B$')
 pyplot.legend()
-#pyplot.plot(x, y, '--', color='red')
-#pyplot.show()
+pyplot.grid()
+pyplot.xlabel(r'$M \mathbin{/} \unit{\meter}$')
+pyplot.ylabel(r'$B \mathbin{/}\unit{\tesla}$')
+pyplot.show()
 pyplot.savefig('build/WerteSpulenPaar20.pdf')
 pyplot.clf()
-
-
-
-
-WerteSpulenPaar15 = np.array(np.genfromtxt('WerteSpulenPaar15.txt'))
+#-----------------------------------
+WerteSpulenPaar15 = np.array(np.genfromtxt('WerteSpulenPaar20.txt'))
 WerteSpulenPaar15[:,1] = WerteSpulenPaar15[:,1] / 1000 #von mT in T umrechnen
-WerteSpulenPaar15[:,1] = WerteSpulenPaar15[:,1] / 100   # von cm in m umrechnen
+WerteSpulenPaar15[:,0] = WerteSpulenPaar15[:,0] / 100 # von cm in m
 
 x = WerteSpulenPaar15[:,0]
 y = WerteSpulenPaar15[:,1]
 
-pyplot.scatter(x, y, s=15)
+pyplot.scatter(x, y, color='blue',s=15, label=r'$B$')
 pyplot.legend()
-#pyplot.plot(x, y, '--', color='red')
-#pyplot.show()
+pyplot.grid()
+pyplot.xlabel(r'$M \mathbin{/} \unit{\meter}$')
+pyplot.ylabel(r'$B \mathbin{/}\unit{\tesla}$')
+pyplot.show()
 pyplot.savefig('build/WerteSpulenPaar15.pdf')
 pyplot.clf()
 
-#x = np.linspace(0, 10, 1000)
-#y = x ** np.sin(x)
-#
-#plt.subplot(1, 2, 1)
-#plt.plot(x, y, label='Kurve')
-#plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
-#plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
-#plt.legend(loc='best')
-#
-#plt.subplot(1, 2, 2)
-#plt.plot(x, y, label='Kurve')
-#plt.xlabel(r'$\alpha \mathbin{/} \unit{\ohm}$')
-#plt.ylabel(r'$y \mathbin{/} \unit{\micro\joule}$')
-#plt.legend(loc='best')
-# in matplotlibrc leider (noch) nicht möglich
-#plt.tight_layout(pad=0, h_pad=1.08, w_pad=1.08)
-#plt.savefig('build/plot.pdf')
