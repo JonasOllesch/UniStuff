f2bc6cb3606a5120b4da8fb0a66c95315aef0704
Lukas Annuss <lukas.annuss@tu-dortmund.de>
2021-12-06 13:20:43 +0100

    git push

--- Versuch_3/V308/plot.py
+++ Versuch_3/V308/plot.py
@@ -19,6 +19,8 @@ pyplot.savefig('build/Hystereseplot.pdf')
 pyplot.clf()
 
 #-----------------------------------
+
+
 WertelangeSpule = np.array(np.genfromtxt('WertelangeSpule.txt'))
 WertelangeSpule[:,1] = WertelangeSpule[:,1] / 1000 #von mT in T umrechnen
 WertelangeSpule[:,0] = WertelangeSpule[:,0] / 100 # von cm in m
@@ -32,10 +34,19 @@ pyplot.grid()
 pyplot.xlabel(r'$M \mathbin{/} \unit{\meter}$')
 pyplot.ylabel(r'$B \mathbin{/}\unit{\tesla}$')
 #pyplot.show()
+#x = np.linspace(0.00001,0.7,1000)
+#pyplot.plot(x,((300*4*np.pi*(10**(-7)))*(1/0.18))*x/x,color='red',label="Theorie")
+#pyplot.legend("Theorie")
+print(((300*4*np.pi*(10**(-7)))*(1/0.18)) , " der exakte Wert")
+
 pyplot.savefig('build/langeSpule.pdf', bbox_inches='tight')
 pyplot.clf()
 
 #-----------------------------------
+x = np.linspace(0,0.13,1000)
+pyplot.plot(x,((100*4*np.pi*(10**(-7)))*(4/2))*((0.0625)**2)/((0.0625**2+x**2)**(3/2))+((100*4*np.pi*(10**(-7)))*(4/2))*((0.0625)**2)/((0.0625**2+(x-0.10)**2)**(3/2)),color='red',label="Theorie")
+pyplot.legend("Theorie")
+
 WerteSpulenPaar10 = np.array(np.genfromtxt('WerteSpulenPaar10.txt'))
 WerteSpulenPaar10[:,1] = WerteSpulenPaar10[:,1] / 1000 #von mT in T umrechnen
 WerteSpulenPaar10[:,0] = WerteSpulenPaar10[:,0] / 100 # von cm in m
@@ -52,6 +63,11 @@ pyplot.ylabel(r'$B \mathbin{/}\unit{\tesla}$')
 pyplot.savefig('build/SpulenPaar10.pdf')
 pyplot.clf()
 #-----------------------------------
+x = np.linspace(0,0.26,1000)
+pyplot.plot(x,((100*4*np.pi*(10**(-7)))*(4/2))*((0.0625)**2)/((0.0625**2+x**2)**(3/2))+((100*4*np.pi*(10**(-7)))*(4/2))*((0.0625)**2)/((0.0625**2+(x-0.20)**2)**(3/2)),color='red',label="Theorie")
+pyplot.legend("Theorie")
+
+
 WerteSpulenPaar20 = np.array(np.genfromtxt('WerteSpulenPaar20.txt'))
 WerteSpulenPaar20[:,1] = WerteSpulenPaar20[:,1] / 1000 #von mT in T umrechnen
 WerteSpulenPaar20[:,0] = WerteSpulenPaar20[:,0] / 100 # von cm in m
@@ -68,14 +84,19 @@ pyplot.ylabel(r'$B \mathbin{/}\unit{\tesla}$')
 pyplot.savefig('build/SpulenPaar20.pdf')
 pyplot.clf()
 #-----------------------------------
+x = np.linspace(0,0.23,1000)
+pyplot.plot(x,((100*4*np.pi*(10**(-7)))*(4/2))*((0.0625)**2)/((0.0625**2+x**2)**(3/2))+((100*4*np.pi*(10**(-7)))*(4/2))*((0.0625)**2)/((0.0625**2+(x-0.15)**2)**(3/2)),color='red',label="Theorie")
+pyplot.legend("Theorie")
+
+
 WerteSpulenPaar15 = np.array(np.genfromtxt('WerteSpulenPaar15.txt'))
 WerteSpulenPaar15[:,1] = WerteSpulenPaar15[:,1] / 1000 #von mT in T umrechnen
 WerteSpulenPaar15[:,0] = WerteSpulenPaar15[:,0] / 100 # von cm in m
-
+#print(WerteSpulenPaar15)
 x = WerteSpulenPaar15[:,0]
 y = WerteSpulenPaar15[:,1]
 
-pyplot.scatter(x, y, color='blue',s=15, label=r'$B$')
+pyplot.scatter(x, y, color='blue',s=10, label=r'$B$')
 pyplot.legend()
 pyplot.grid()
 pyplot.xlabel(r'$M \mathbin{/} \unit{\meter}$')
@@ -85,25 +106,28 @@ pyplot.savefig('build/SpulenPaar15.pdf')
 pyplot.clf()
 
 #theorie für die lange Spule
-BinlangeSpule = 4*np.pi*300*4/(0.18)
+#BinlangeSpule = 4*np.pi*300*4/(0.18)
 #theorie für das Spulenpaar für 10
-BSpulenpaar = [0,1,2,3,4,5,6,7,8,9,10,11,12,13]
-x = [0,1,2,3,4,5,6,7,8,9,10,11,12,13]
-for i in range(0,14):
-    x[i]= x[i]/100
-print(x)
-for i in range(0,14):
-    BSpulenpaar[i] = ((100*4*np.pi*10**(-7))*4/2)*(0.0625)**2*x[i]/((0.0625**2+x[i]**2)**(3/2))
-pyplot.scatter(x, BSpulenpaar, color='red',s=15, label=r'$Theorie$')
+#BSpulenpaar = [0,1,2,3,4,5,6,7,8,9,10,11,12,13]
 
-x = WerteSpulenPaar10[:,0]
-y = WerteSpulenPaar10[:,1]
-
-pyplot.scatter(x, y, color='blue',s=15, label=r'$B$')
-pyplot.legend()
-pyplot.grid()
-pyplot.xlabel(r'$M \mathbin{/} \unit{\meter}$')
-pyplot.ylabel(r'$B \mathbin{/}\unit{\tesla}$')
+#for i in range(0,14):
+#    x[i]= x[i]/100
+#print(x)
+#for i in range(0,14):
+#    BSpulenpaar[i] = ((100*4*np.pi*(10**(-7)))*(4/2))*((0.0625)**2)/((0.0625**2+x[i]**2)**(3/2))+((100*4*np.pi*(10**(-7)))*(4/2))*((0.0625)**2)/((0.0625**2+(x[i]-0.1)**2)**(3/2))
+#pyplot.scatter(x, BSpulenpaar, color='red',s=15, label=r'$Theorie$')
+#x = np.linspace(0,0.23,1000)
+#pyplot.plot(x,((100*4*np.pi*(10**(-7)))*(4/2))*((0.0625)**2)/((0.0625**2+x**2)**(3/2))+((100*4*np.pi*(10**(-7)))*(4/2))*((0.0625)**2)/((0.0625**2+(x-0.15)**2)**(3/2)),color='red',label="Theorie")
+#pyplot.legend("Theorie")
+#
+#x = WerteSpulenPaar15[:,0]
+#y = WerteSpulenPaar15[:,1]
+#
+#pyplot.scatter(x, y, color='blue',s=15, label=r'$B$')
+#pyplot.legend()
+#pyplot.grid()
+#pyplot.xlabel(r'$M \mathbin{/} \unit{\meter}$')
+#pyplot.ylabel(r'$B \mathbin{/}\unit{\tesla}$')
 #pyplot.show()
-pyplot.savefig('build/Test.pdf')
-pyplot.clf()
+#pyplot.savefig('build/15.pdf')
+#pyplot.clf()
