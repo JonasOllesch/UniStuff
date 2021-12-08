13da41789bbbabece66c66bb89cfc6fed9c9b265
Lukas Annuss <lukas.annuss@tu-dortmund.de>
2021-12-08 19:51:35 +0100

    .

--- Versuch_4/V355/plot.py
+++ Versuch_4/V355/plot.py
@@ -1,2 +1,23 @@
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as pyplot
+import pandas as pd
 import numpy as np
+from uncertainties import ufloat
+import uncertainties.unumpy as unp
+from pandas import read_csv
+import math
+from scipy.optimize import curve_fit
+from numpy import arange
+
+# Werte einlesen und in Si Einheiten konvertieren
+Messung_a = np.array(np.genfromtxt('a.txt'))
+Messung_a[:,0]= Messung_a[:,0]*(10**-9)
+
+Messung_b = np.array(np.genfromtxt('b.txt'))
+Messung_b[:,0]= Messung_b[:,0]*(10**-9)
+Messung_b[:,1]= Messung_b[:,1]*(10**3)
+Messung_b[:,2]= Messung_b[:,2]*(10**3)
+
+Messung_c = np.array(np.genfromtxt('c.txt'))
+Messung_c[:,0]= Messung_c[:,0]*(10**-9)
+Messung_c[:,1]= Messung_c[:,1]*(10**-3)
+Messung_c[:,2]= Messung_c[:,2]*(10**-3)
