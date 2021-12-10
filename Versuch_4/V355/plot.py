d75bb64c7e97b1f0c52a51a51fb93ff2ab471d8e
Lukas Annuss <lukas.annuss@tu-dortmund.de>
2021-12-10 17:21:08 +0100

    Auswertung bis auf c

--- Versuch_4/V355/plot.py
+++ Versuch_4/V355/plot.py
@@ -28,3 +28,64 @@ Messung_c = np.array(np.genfromtxt('c.txt'))
 Messung_c[:,0]= Messung_c[:,0]*(10**-9)
 Messung_c[:,1]= Messung_c[:,1]*(10**-3)
 Messung_c[:,2]= Messung_c[:,2]*(10**-3)
+#theoretische Werte für ν- und ν+ berechnen
+
+C_k = [12,9.99,8.18,6.86,4.74,2.86,2.19,0.997]
+for i in range(0,len(C_k)):
+    C_k[i] = C_k[i]*(10**-9)
+
+L = ufloat(23.95*(10**-3),0.05*(10**-3))
+C_sp = ufloat(0.028*(10**-9),0.028*(10**-9)*(1/20)) #die Fehler habe ich jetzt einfach auf 5% geschätzt wenn du andere haben möchtest einfach ändern :)
+C = ufloat (0.7935*(10**-9),0.7935*(10**-9)*(1/20))
+
+#ν-  mit Formel 4.14 aus dem Altprotokoll
+dieKlammer = [0] * 8
+v_mt = [0]*8
+for i in range(0,len(v_mt)):
+    dieKlammer[i] = (1/C + 2/C_k[i])**-1 +C_sp
+    v_mt[i] = (2*np.pi*(unp.sqrt(L*dieKlammer[i])))**-1
+#for i in range(0,len(v_mt)):
+#    print(v_mt[i]/1000)
+
+#mit Formel 4.13 aus dem Altprotokoll
+dieWurzel = [0]*8
+v_pt = [0]*8
+for i in range(0,len(v_pt)):
+    dieWurzel[i] = unp.sqrt(L*(C+C_sp))
+    v_pt[i]      = (2*np.pi*dieWurzel[i])**-1
+del dieWurzel
+#for i in range(0,len(v_pt)):
+#    print(v_pt[i]/1000)
+
+# das theoretische Verhältnis zwischen Schwingung und Schwebugn
+n_t =[0]*8
+for i in range(0,len(n_t)):
+    n_t[i] = (v_pt[i]+v_mt[i])/(2*(v_mt[i]-v_pt[i]))
+#print(n_t, " n_t")
+Arel = [0]*8
+for i in range(0,len(Arel)):
+    Arel[i] = unp.nominal_values(abs((Messung_a[i][1]-n_t[i])/n_t[i]))
+#print(Arel)
+
+#die Aufgabe b
+Brel_vp= [0]*8
+Brel_vm= [0]*8
+
+for i in range(0,len(Brel_vp)):
+    Brel_vp[i] = unp.nominal_values(abs((Messung_b[i][1]-v_pt[i])/v_pt[i]))
+    Brel_vm[i] = unp.nominal_values(abs((Messung_b[i][2]-v_mt[i])/v_mt[i]))
+
+print(Brel_vm)  #irgendwie steht hier noch ein "array im array lol"
+print('\n')
+print(Brel_vp)  #irgendwie steht hier noch ein "array im array lol"
+#die Aufgabe C
+Messung_t = [[1,2],[1,2],[1,2],[1,2],[1,2],[1,2],[1,2],[1,2]] # die Messung der Zeit
+for i in range(0,8):
+    Messung_t[i][0] = Messung_c[i][1]
+    Messung_t[i][1] = Messung_c[i][2]
+
+    Messung_t[i][0] = Messung_t[i][0]**-1
+    Messung_t[i][1] = Messung_t[i][1]**-1
+
+    
+    
