1cab080e17f7b3f2391e4e3ddf8c361023c1ba9c
Lukas Annuss <lukas.annuss@tu-dortmund.de>
2021-12-31 16:48:48 +0100

    yey

--- Besselfunktion/Besselfunktion.py
+++ Besselfunktion/Besselfunktion.py
@@ -1,16 +1,26 @@
+#das ist die Zusatzaufgabe in Mathe \ die Plots sehen eigenlich ganz gut aus \ die Suche nach den Nullstellen ist rudimentär aber funktioniert ganz okay
 import matplotlib.pyplot as pyplot
 import numpy as np
 import scipy
 import scipy.special
+gen = 10000
 
 def func(x):
     return  2 / x
 
-xdata = np.linspace(1, 100, 10000, endpoint=False)# bei großen z asymtotisch gegen null
+xdata = np.linspace(1, 20, gen, endpoint=False)# bei großen z asymtotisch gegen null
 
 ydata =  scipy.special.jv(1,xdata)
 
 pyplot.plot(xdata, ydata,color ='green', label="1.Ordnung")
+
+
+for i in range(0,gen-1):
+    if abs(scipy.special.jv(1,xdata[i])) < abs(scipy.special.jv(1,xdata[i+1])):
+        if abs(scipy.special.jv(1,xdata[i])) < abs(scipy.special.jv(1,xdata[i-1])):
+            print(" ",xdata[i], " i")
+            print('\n')
+            i= i+2
 
 y2data = scipy.special.jv(2,xdata)
 pyplot.plot(xdata, y2data,color ='black', label="2.Ordnung K")
