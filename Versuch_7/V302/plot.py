eb3367d522a0976b50a16fa9ab6973b1f4d32df9
Lukas Annuss <lukas.annuss@tu-dortmund.de>
2022-01-17 11:10:24 +0100

    .

--- Versuch_7/V302/plot.py
+++ Versuch_7/V302/plot.py
@@ -1,5 +1,7 @@
 import matplotlib.pyplot as pyplot
 import numpy as np
+import uncertainties as unp
+from uncertainties import ufloat
 
 output = ("build/Auswertung")    
 my_file = open(output + '.txt', "w")
@@ -27,11 +29,20 @@ def theorie_kurve(o):
     tmp2 = (1-o**2)**2
     tmp3 = 9*(o**2)   
     return  np.sqrt((1/9)*(tmp/(tmp2+tmp3)))
+
+
+Messung_a = np.array(np.genfromtxt('Kapazitätsbrücke_a.txt'))
+Messung_a[:,1]= Messung_a[:,1]/1000
+Messung_b = np.array(np.genfromtxt('Kapazitätsbrücke_b.txt'))
+Messung_b[:,1] = Messung_b[:,1]/1000
+
+
+
 #-------------------------------------------------------------------------------
 
 #Wheatstonemessbrücke
 #zu Wert 12
-R1_1 = r_x_berchnen(1000, 282, 718)
+R1_1 = r_x_berchnen(ufloat(1000,0.03), 282, 718)
 R1_2 = r_x_berchnen(664, 371, 629)
 
 
@@ -68,23 +79,37 @@ C2_1 = (750*10**-9)*(368/632)
 R2_2 = r_x_berchnen(347, 582, 418)
 C2_2 = (597*10**-9)*(418/582)
 
+#Wien-Robinson-Brücke
+
+
 x1=np.linspace(0.1,2,1000)
 x = np.linspace(2,100,1000)
 y1 = theorie_kurve(x1)
 y= theorie_kurve(x)
 pyplot.plot(x1, y1,color='blue')
 pyplot.plot(x, y,color='blue',label='Theorie')
-pyplot.xlim(0.1,100)
-pyplot.ylim(0,0.35)
+#pyplot.xlim(0.1,100)
+#pyplot.ylim(0,0.35)
 pyplot.xscale('log')
 pyplot.xlabel(r'$\log{Ω}$')
 pyplot.ylabel(r'$\frac{U_{Br}}{U_S}$')
 
 
+U_s = np.mean(Messung_b[:,1])
+
+y = Messung_a[:,1]/U_s
+x= Messung_a[:,0]/161
+pyplot.scatter(x, y,s=8, c='red',marker='x')
+
+v_0_theorie = 1/(1000*(993*10**-9))
+real_Abw_v_0 = (161-v_0_theorie)/v_0_theorie 
+
 pyplot.legend()
 pyplot.grid()
 pyplot.tight_layout()
 pyplot.savefig('build/Theorie.pdf')
-pyplot.clf()
+
+
 
 #---------------------------------------------------------------------------------
+writeW(U_s, "U_s")
