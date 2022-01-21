45256132d2daefb0aa2595f001534fd292b5a64c
Lukas Annuss <lukas.annuss@tu-dortmund.de>
2022-01-21 10:36:29 +0100

    .

---
+++ Versuch_8/V353/plot.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+output = ("build/Auswertung")    
+my_file = open(output + '.txt', "w")
+def writeW(Wert,Beschreibung):
+    my_file.write(str(Beschreibung))
+    my_file.write('\n')
+    try:
+        for i in Wert:
+            my_file.write(str(i))
+            my_file.write('\n')
+    except:
+        my_file.write(str(Wert))
+        my_file.write('\n')
+
+    return 0
+
+x = np.linspace(0, 10, 1000)
+y = x ** np.sin(x)
+
+
