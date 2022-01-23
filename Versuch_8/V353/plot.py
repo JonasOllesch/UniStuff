21ab1cf5814a868e342516e0338cb305dfd32b14
Lukas Annuss <lukas.annuss@tu-dortmund.de>
2022-01-23 15:38:26 +0100

    .

--- Versuch_8/V353/plot.py
+++ Versuch_8/V353/plot.py
@@ -47,12 +47,9 @@ y_ausgleich_a = func_pol1(x_ausgleich_a, popt_a[0], popt_a[1])
 pyplot.plot(x_ausgleich_a, y_ausgleich_a,label='lineare Regression')
 a1 = ufloat(popt_a[0],np.sqrt(pcov_a[0][0]))
 b1 = ufloat(popt_a[1],np.sqrt(pcov_a[1][1]))
-print(a1)
-print(b1)
-t = 1/a1
-writeW(a1, "Erste Messung a1")
-writeW(b1, "Erste Messung b1")
-writeW(t, "RC")
+
+tau_1 = a1
+
 pyplot.ylabel(r'$\ln{(\frac{U_c}{U_0})}$')
 pyplot.xlabel(r'${T}  \mathbin{/} \unit{\second}$')
 
@@ -71,14 +68,17 @@ popt_b, pcov_b = curve_fit(func_13, x2data, y2data)#U_0 sollte doch ein Messwert
 x_ausgleich_b = np.logspace(1,5)
 y_ausgleich_b = func_13(x_ausgleich_b, popt_b[0],popt_b[1])
 pyplot.scatter(x2data, y2data,s=8, c='red',marker='x',label="Messwerte")
-pyplot.plot(x_ausgleich_b, y_ausgleich_b,label='lineare Regression')
+pyplot.plot(x_ausgleich_b, y_ausgleich_b,label='Ausgleichsfunktion')
 pyplot.xlim(10,50000)
 pyplot.ylim(0,10)
 tmp =np.sqrt(popt_b[0])
 tmp2 =np.sqrt(pcov_b[0][0])
-writeW(ufloat(tmp,tmp2), "RC 2")
-writeW(popt_b[0], "RC")
-writeW(popt_b[1], "U_0")
+RC_2 = ufloat(tmp,tmp2)
+tau_2 = 1/RC_2
+
+
+
+
 pyplot.xlabel(r'$f \mathbin{/} \unit{\hertz} $')
 pyplot.ylabel(r'$ U \mathbin{/} \unit{\volt} $')
 
@@ -93,7 +93,7 @@ Messung_b2 = np.empty((8,4),float)
 for i in range(15,23):
     for j in range(0,4):
         Messung_b2[i-15][j]= Messung_b[i][j]
-writeW(Messung_b2, "b2")
+
 
 phi = (Messung_b2[:,2]/Messung_b2[:,3]) * 2*np.pi
 frequenz = Messung_b2[:,0]
@@ -101,14 +101,12 @@ pyplot.scatter(Messung_b2[:,0],phi,s=8, c='red',marker='x',label="Messwerte")
 
 pyplot.xlabel(r'$f \mathbin{/} \unit{\hertz} $')
 pyplot.ylabel(r'$ \phi $')
-writeW(Messung_b2, "b2")
-writeW(phi, "phi")
+
 popt_b2, pcov_b2 = curve_fit(func_11, frequenz, phi)
 x_ausgleich_b2 = np.logspace(3,5)
 y_ausgleich_b2 = func_11(x_ausgleich_b2, popt_b2[0])
 pyplot.plot(x_ausgleich_b2, y_ausgleich_b2,c='blue',label='Ausgeleichsfunktion')
-writeW(-popt_b2[0],"RC b2")
-writeW(-1/popt_b2[0],"1/RC b2")
+
 pyplot.xscale('log')
 pyplot.tight_layout()
 pyplot.legend()
@@ -125,3 +123,6 @@ pyplot.tight_layout()
 pyplot.legend()
 pyplot.savefig('build/Graph_c.pdf')
 pyplot.clf()
+#----------------------------------------
+#Auswertung
+writeW(tau_1/tau_2, "tau_1/tau_2")
