c16c5b324e215ff5985ccc993c488a439ab92e21
Lukas Annuss <lukas.annuss@tu-dortmund.de>
2022-04-05 19:55:44 +0200

    .

--- Praktikum_2/Versuch_1/V407/plot.py
+++ Praktikum_2/Versuch_1/V407/plot.py
@@ -3,3 +3,9 @@ import numpy as np
 from scipy.optimize import curve_fit
 from uncertainties import ufloat
 
+#output = ("Messung_1")    
+#my_file = open(output + '.txt', "w")
+#
+#for i in range(0,21):
+#    my_file.write(str(i*5))
+#    my_file.write('\n')
