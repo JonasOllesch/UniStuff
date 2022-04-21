a4ffe9e01562089dcbeca6f71afba44133519a87
Lukas Annuss <lukas.annuss@tu-dortmund.de>
2022-04-21 08:24:19 +0200

    .

--- Praktikum_2/Versuch_3/V703/plot.py
+++ Praktikum_2/Versuch_3/V703/plot.py
@@ -23,14 +23,12 @@ Messung_1 = np.array(np.genfromtxt('Messdaten/Messung_1&4.txt'))
 Messung_1[:,1] = Messung_1[:,1]*1e-6
 
 
-#Aufgabe a
+#Aufgabe a der Plateaubereich geht von 350 bis 550. Das habe ich jetzt so bestimmt. Du kannst das aber gerne ändern, wenn du möchtest. 
 plt.scatter(Messung_1[:,0],Messung_1[:,1],s=8, c='blue',label="Spannung - Stromstärke")
 plt.xlabel(r'$U \mathbin{/} \unit{\volt}$')
 plt.ylabel(r'$I\mathbin{/} \unit{\ampere}$')
 plt.tight_layout()
 plt.legend()
-
-
 plt.grid()
 plt.savefig('build/Graph_a.pdf')
 plt.clf()
@@ -39,7 +37,6 @@ plt.clf()
 plt.scatter(Messung_1[:,0],Messung_1[:,2],s=8, c='red',label="Spannung - Teilchenzahl")
 plt.xlabel(r'$ I\mathbin{/} \unit{\ampere} $')
 plt.ylabel(r'$ \text{Teilchenzahl} $')
-
 x_plateau = np.linspace(330,700,1000)
 koeffizienten_plateau = np.polyfit(Messung_1[:22,0],Messung_1[:22,2],1)
 y_plateau = np.polyval(koeffizienten_plateau,x_plateau)
@@ -55,5 +52,15 @@ plt.savefig('build/Graph_b.pdf')
 plt.clf()
 
 #Aufgabe c
+#zwei Quellen Methoden
 Totzeit = (22084+18864-40352)/(2*22084*18864) 
 writeW(Totzeit, "Näherung der Todzeit")
+
+
+#Aufgabe d
+delta_Q = np.zeros(36)
+delta_Q[:] = 120*Messung_1[:,1]/Messung_1[:,2]
+writeW(delta_Q, "delta_q")
+delta_Q_in_elementarladung = np.zeros(36)
+delta_Q_in_elementarladung[:] = delta_Q[:]/(1.602176634*1e-16)
+writeW(delta_Q_in_elementarladung, "delta_Q_in_elementarladung")
