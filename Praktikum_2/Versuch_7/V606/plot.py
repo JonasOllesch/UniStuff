7fecee495e3cb5381e3aad063412e58307146874
Lukas Annuss <lukas.annuss@tu-dortmund.de>
2022-05-22 16:01:01 +0200

    .

--- Praktikum_2/Versuch_7/V606/plot.py
+++ Praktikum_2/Versuch_7/V606/plot.py
@@ -34,6 +34,7 @@ Messung_2_a[:,1] = Messung_2_a[:,1]*0.1
 Messung_2_a[:,2] = Messung_2_a[:,2]*5*(10**(-3))
 Messung_2_a[:,3] = Messung_2_a[:,3]*0.1
 
+
 Messung_2_b = np.array(np.genfromtxt('Messung_2_b.txt'))
 Messung_2_b[:,0] = Messung_2_b[:,0]*5*(10**(-3))
 Messung_2_b[:,1] = Messung_2_b[:,1]*0.1
@@ -65,39 +66,51 @@ plt.savefig('build/Graph_a.pdf')
 plt.clf()
 
 Q_real_Dy = 0.01438/(0.155*7800)
-Q_real_Nd = 0.0766/(0.16*7240)
 Q_real_Gd = 0.1020/(0.159*7400)
-writeW(Q_real_Dy, "Q_real_Dy2O3")
-writeW(Q_real_Nd, "Q_real_Nd2O3")
-writeW(Q_real_Gd, "Q_real_Gd2O3")
+Q_real_Nd = 0.0766/(0.16*7240)
 
-suszep_aus_r = np.zeros((3,3),dtype=float)#Dy2O3  Nd2O3 Gd2O3
+writeW(Q_real_Dy, "Q_real_Dy2O3")
+writeW(Q_real_Gd, "Q_real_Gd2O3")
+writeW(Q_real_Nd, "Q_real_Nd2O3")
+
+suszep_aus_r = np.zeros((3,3),dtype=float)#Dy2O3  Gd2O3 Nd2O3
 suszep_aus_r[:,0] = (2*abs(Messung_2_a[:,0]-Messung_2_a[:,2])*(86.6*10**(-6)))/(998*Q_real_Dy)
-suszep_aus_r[:,1] = (2*abs(Messung_2_b[:,0]-Messung_2_b[:,2])*(86.6*10**(-6)))/(998*Q_real_Nd)
-suszep_aus_r[:,2] = (2*abs(Messung_2_c[:,0]-Messung_2_c[:,2])*(86.6*10**(-6)))/(998*Q_real_Gd)
+suszep_aus_r[:,1] = (2*abs(Messung_2_b[:,0]-Messung_2_b[:,2])*(86.6*10**(-6)))/(998*Q_real_Gd)
+suszep_aus_r[:,2] = (2*abs(Messung_2_c[:,0]-Messung_2_c[:,2])*(86.6*10**(-6)))/(998*Q_real_Nd)
 writeW(suszep_aus_r, "suszep_aus_r")
 
 suszep_aus_u = np.zeros((3,3),dtype=float)
 suszep_aus_u[:,0] = 4*(86.6*10**(-6))*abs(Messung_2_a[:,1]-Messung_2_a[:,3])/(Q_real_Dy)
-suszep_aus_u[:,1] = 4*(86.6*10**(-6))*abs(Messung_2_b[:,1]-Messung_2_b[:,3])/(Q_real_Nd)
-suszep_aus_u[:,2] = 4*(86.6*10**(-6))*abs(Messung_2_c[:,1]-Messung_2_c[:,3])/(Q_real_Gd)
+suszep_aus_u[:,1] = 4*(86.6*10**(-6))*abs(Messung_2_b[:,1]-Messung_2_b[:,3])/(Q_real_Gd)
+suszep_aus_u[:,2] = 4*(86.6*10**(-6))*abs(Messung_2_c[:,1]-Messung_2_c[:,3])/(Q_real_Nd)
 
 suszep_mean_std = np.zeros((3,4),dtype = float)
 
 suszep_aus_r_Dy203_mean = np.mean(suszep_aus_r[:,0])
 suszep_aus_r_Dy203_std = np.std(suszep_aus_r[:,0])
-suszep_aus_r_Nd203_mean = np.mean(suszep_aus_r[:,1])
-suszep_aus_r_Nd203_std = np.std(suszep_aus_r[:,1])
-suszep_aus_r_Gd203_mean = np.mean(suszep_aus_r[:,2])
-suszep_aus_r_Gd203_std = np.std(suszep_aus_r[:,2])
+suszep_aus_r_Gd203_mean = np.mean(suszep_aus_r[:,1])
+suszep_aus_r_Gd203_std = np.std(suszep_aus_r[:,1])
+suszep_aus_r_Nd203_mean = np.mean(suszep_aus_r[:,2])
+suszep_aus_r_Nd203_std = np.std(suszep_aus_r[:,2])
 
 suszep_aus_u_Dy203_mean = np.mean(suszep_aus_u[:,0])
 suszep_aus_u_Dy203_std = np.std(suszep_aus_u[:,0])
-suszep_aus_u_Nd203_mean = np.mean(suszep_aus_u[:,1])
-suszep_aus_u_Nd203_std = np.std(suszep_aus_u[:,1])
-suszep_aus_u_Gd203_mean = np.mean(suszep_aus_u[:,2])
-suszep_aus_u_Gd203_std = np.std(suszep_aus_u[:,2])
+suszep_aus_u_Gd203_mean = np.mean(suszep_aus_u[:,1])
+suszep_aus_u_Gd203_std = np.std(suszep_aus_u[:,1])
+suszep_aus_u_Nd203_mean = np.mean(suszep_aus_u[:,2])
+suszep_aus_u_Nd203_std = np.std(suszep_aus_u[:,2])
 
 
 
 writeW(suszep_aus_u, "suszep_aus_u")
+print(Messung_2_a)
+print(Messung_2_b)
+print(Messung_2_c)
+
+print(abs(Messung_2_a[:,0]-Messung_2_a[:,2]), " delta ohm")
+print(abs(Messung_2_b[:,0]-Messung_2_b[:,2]), " delta ohm")
+print(abs(Messung_2_c[:,0]-Messung_2_c[:,2]), " delta ohm")
+
+writeW(abs(Messung_2_a[:,1]-Messung_2_a[:,3]), "delta U Dy2o3")
+writeW(abs(Messung_2_b[:,1]-Messung_2_b[:,3]), "delta U Gd2o3")
+writeW(abs(Messung_2_c[:,1]-Messung_2_c[:,3]), "delta U Nd2o3")
