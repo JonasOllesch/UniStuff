78e28470102a3c4f1dce239ff53b73cb61dbaefb
Lukas Annuss <lukas.annuss@tu-dortmund.de>
2022-06-05 16:07:20 +0200

    Messdaten

--- Praktikum_2/Versuch_9/V504/plot.py
+++ Praktikum_2/Versuch_9/V504/plot.py
@@ -6,3 +6,18 @@ from uncertainties import ufloat
 # Die Kennlinien bitte in 'kennlinien.pdf' speichern, sonst muss der Code in auswertung.tex noch geändert werden '^^
 # Den Fit (Aufgabenteil b)) für Messreihe 5 als 'linregraumladung.pdf' speichern (oder halt in der Auswertung ändern :D)
 # Der Fit zum Anlaufstromgebiet sollte irgendwie 'reganlaufstrom.pdf' heißen :D
+
+output = ("Auswertung")   
+my_file = open(output + '.txt', "w") 
+def writeW(Wert,Beschreibung):
+    my_file.write(str(Beschreibung))
+    my_file.write('\n')
+    try:
+        for i in Wert:
+            my_file.write(str(i))
+            my_file.write('\n')
+    except:
+        my_file.write(str(Wert))
+        my_file.write('\n')
+
+    return 0
