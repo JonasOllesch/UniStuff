160797165b2bebc2c50076bc1abeb372b1134426
Jonas Ollesch <jonas.ollesch@tu-dortmund.de>
2022-06-05 14:47:53 +0200

    Gerüst für die Auswertung ist fertig, nur noch die ganzen Werte müssten eingetragen werden

--- Praktikum_2/Versuch_9/V504/plot.py
+++ Praktikum_2/Versuch_9/V504/plot.py
@@ -3,3 +3,6 @@ import numpy as np
 from scipy.optimize import curve_fit
 from uncertainties import ufloat
 
+# Die Kennlinien bitte in 'kennlinien.pdf' speichern, sonst muss der Code in auswertung.tex noch geändert werden '^^
+# Den Fit (Aufgabenteil b)) für Messreihe 5 als 'linregraumladung.pdf' speichern (oder halt in der Auswertung ändern :D)
+# Der Fit zum Anlaufstromgebiet sollte irgendwie 'reganlaufstrom.pdf' heißen :D
