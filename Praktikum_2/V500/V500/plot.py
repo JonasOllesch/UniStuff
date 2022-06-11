cf1c183ab080cce65b5ce7396d16952c4a018436
Jonas Ollesch <jonas.ollesch@tu-dortmund.de>
2022-06-11 14:55:34 +0200

    Durchführung und Gerüst für Auswertung auch fertig

--- Praktikum_2/V500/V500/plot.py
+++ Praktikum_2/V500/V500/plot.py
@@ -3,3 +3,6 @@ import numpy as np
 from scipy.optimize import curve_fit
 from uncertainties import ufloat
 
+# Plot mit Photostrom gegen Bremsspannung sollte 'photostrom.pdf' heißen, wenn du da noch einen besseren Namen findest (nicht 'graph1.pdf' ;)), kannst du das ja sonst auch in der Auswertung und hier anders nennen.
+# Plot der Gegenspannungen gegen die Frequenzen bitte in 'gegspanngegfreq.pdf' :)
+# Plot für gelbe Kennlinie bitte unter 'kennliniegelb.pdf' :D
