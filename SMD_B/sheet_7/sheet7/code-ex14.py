91eb46b6af477012b799746d164968b9b0aa5223
Jonas Ollesch <jonas.ollesch@tu-dortmund.de>
2022-12-13 14:13:26 +0100

    .

---
+++ SMD_B/sheet_7/sheet7/code-ex14.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def calc_likelihood(data):
+    return 
+
+
+
+data = np.array([4135, 4202, 4203, 4218, 4227, 4231, 4310])
+new_data = np.array([4135, 4202, 4203, 4218, 4227, 4231, 4310,4402])
