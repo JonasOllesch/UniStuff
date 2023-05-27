37876281476cd217af485a58ff0ba709f789c4a0
Jonas Ollesch <jonas.ollesch@tu-dortmund.de>
2023-05-27 14:57:38 +0200

    .

---
+++ Praktikum_3/V48/V48/plot.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+from uncertainties import ufloat
+from uncertainties import correlated_values
+import uncertainties.unumpy as unp
+import copy
+from scipy import constants
