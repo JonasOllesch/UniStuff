1261b03bf27feaad5112e2776d7158d74046ffb9
Lukas Annuss <lukas.annuss@tu-dortmund.de>
2023-06-06 17:00:23 +0200

    .

--- Praktikum_3/V48/V48/plot.py
+++ Praktikum_3/V48/V48/plot.py
@@ -10,44 +10,81 @@ from scipy import constants
 def methode_eins(T,c,W):
     return c - W/(constants.Boltzmann*T)
 
-Zeit, Temperatur, Strom, b = np.genfromtxt('Messdaten/Not_my_data.txt',encoding='unicode-escape',unpack=True)
-del b
-Strom = Strom*(1e-12)
-Daten_länge = len(Zeit)
-b = np.zeros(Daten_länge)
+#def Hintergrund_func(x,a,b,c):
+#    return np.exp(a*x+b)+c
+#
+def Hintergrund_func(x,a,b):
+    return a*x+b
+
+def pol1(x,a,b):
+    return a*x+b
+#my_file = open("Messdaten/Messung1.txt", "a") 
+#for i in range(0,68):
+#    my_file.write(str(i))
+#    my_file.write('\n')
+
+Zeit1, Temperatur1, Strom1 = np.genfromtxt('Messdaten/Messung1.txt',encoding='unicode-escape',unpack=True)
+Temperatur1 = Temperatur1+constants.zero_Celsius
+Strom1 = Strom1*(1e-11)
+Daten_länge = len(Zeit1)
+Heizrate1 = np.zeros(Daten_länge)
 for i in range(1,Daten_länge):
-    b[i] = Temperatur[i]-Temperatur[i-1]
+    Heizrate1[i] = Temperatur1[i]-Temperatur1[i-1]
+
+mask1 = np.logical_or(Temperatur1 <= -35+ constants.zero_Celsius, np.logical_and(Temperatur1 >= 0+ constants.zero_Celsius, Temperatur1 <= 6+ constants.zero_Celsius))
+background_Tem = Temperatur1[mask1]
+background_Str = Strom1[mask1]
+
+x1 = np.linspace(-50+ constants.zero_Celsius,50+ constants.zero_Celsius,1000)
+popt, pcov = curve_fit(Hintergrund_func,background_Tem,background_Str,maxfev=800)
+para = correlated_values(popt,pcov)
+y1 = Hintergrund_func(x1,*para)
+plt.plot(unp.nominal_values(x1),unp.nominal_values(y1)*1e12,color='red')
+
+mask2 = np.logical_and(Temperatur1 >= -35+ constants.zero_Celsius, Temperatur1 <= 6+ constants.zero_Celsius)
+signal_Tem = Temperatur1[mask2]
+signal_Str = Strom1[mask2]
+signal_Str = signal_Str-Hintergrund_func(signal_Tem,*para)
+
+plt.scatter(Temperatur1,Strom1*1e12,s=6,label='Messwerte')
+plt.scatter(background_Tem,background_Str*1e12,s=6,c='green',label='Hintergrund')
 
 
-Temperatur = Temperatur+constants.zero_Celsius
-
-plt.scatter(Temperatur,Strom*(1e12),marker='x',s=3,label='Messdaten')
-#plt.plot(unp.nominal_values(x_fit),unp.nominal_values(y_fit),color='red',label='Fit')
-plt.xlabel(r"$ T  \mathbin{/}  \unit{\celsius} $")
-plt.ylabel(r"$I \mathbin{/} \unit{\pico\ampere} $")
+plt.xlabel(r"$T \mathbin{/} \unit{\kelvin}$")
+plt.ylabel(r"$I \mathbin{/} \unit{\pico\ampere}$")
+plt.ylim(0,8)
 plt.grid(linestyle = ":")
 plt.tight_layout()
 plt.legend()
-plt.savefig('build/Messdaten.pdf')
+plt.savefig('build/Messung1.pdf')
 plt.clf()
 
-Strom = np.log(Strom)
-#methode_eins(T,c,W)
-popt_m_1, pcov_m_1 = curve_fit(methode_eins,Temperatur[1:Daten_länge-4],Strom[1:Daten_länge-4],p0=[5,10])
-para_m_1 = correlated_values(popt_m_1, pcov_m_1)
-x_fit = np.linspace(200,325,500)
-y_fit = methode_eins(x_fit,*para_m_1)
-print(para_m_1)
-y_fit_test = methode_eins(x_fit,-14.556,4.156)
-#plt.plot(x_fit,unp.nominal_values(y_fit_test),color='green',label='parameter test')
-
-
-plt.scatter(Temperatur,Strom,marker='x',s=3,label='Messdaten')
-plt.plot(x_fit,unp.nominal_values(y_fit),color='red',label='Fit')
-plt.xlabel(r"$ T  \mathbin{/}  \unit{\celsius} $")
-plt.ylabel(r"$I \mathbin{/} \unit{\pico\ampere} $")
+#print(signal_Tem)
+plt.errorbar(signal_Tem,unp.nominal_values(signal_Str*1e12),yerr=unp.std_devs(signal_Str*1e12),fmt='x',c='darkorange',label='Bereinigte Messwerte')
+plt.xlabel(r"$T \mathbin{/} \unit{\kelvin}$")
+plt.ylabel(r"$I \mathbin{/} \unit{\pico\ampere}$")
+plt.ylim(0,8)
 plt.grid(linestyle = ":")
 plt.tight_layout()
 plt.legend()
-plt.savefig('build/log_Messdaten_m1.pdf')
+plt.savefig('build/Messung1_bereinigt.pdf')
+plt.clf()
+
+
+#Ansatz über die Polarisation
+plt.scatter(1/(constants.k*unp.nominal_values(signal_Tem[signal_Str>0]))*1e-18,np.log(unp.nominal_values(signal_Str[signal_Str>0])),marker='x',c='darkorange',label='Werte')
+plt.scatter(1/(constants.k*unp.nominal_values(signal_Tem[1:12]))*1e-18,np.log(unp.nominal_values(signal_Str[1:12])),marker='x',c='red',label='Gefittete Werte')
+x_pol1 = np.linspace(1/(constants.k*unp.nominal_values(signal_Tem[11])),1/(constants.k*unp.nominal_values(signal_Tem[1])),2)
+popt, pcov = curve_fit(pol1,xdata=1/(constants.k*unp.nominal_values(signal_Tem[1:12])),ydata=np.log(unp.nominal_values(signal_Str[1:12])))
+para_pol1 = correlated_values(popt, pcov)
+
+y_pol1 = pol1(x_pol1,*para_pol1)
+plt.plot(x_pol1*1e-18,unp.nominal_values(y_pol1),color='red')
+plt.ylabel(r"$ ln\left(\dfrac{I}{\unit{\ampere}} \right) $")
+plt.xlabel(r"$ \dfrac{1}{k_B T} \mathbin{/} \dfrac{a}{\unit{\joule}}$")
+
+plt.grid(linestyle = ":")
+plt.tight_layout()
+plt.legend()
+plt.savefig('build/pol1.pdf')
 plt.clf()
