a2f494cc84661acdb7be9ae828ec939e1c1f4273
Lukas Annuss <lukas.annuss@tu-dortmund.de>
2023-07-01 23:27:56 +0200

    .

--- Praktikum_3/V21/V21/plot.py
+++ Praktikum_3/V21/V21/plot.py
@@ -34,7 +34,7 @@ B1_ges = Magnetfeld_Spule(11,P1_V*0.1,0.1639) + Magnetfeld_Spule(154, U_hor1*2, 
 popt, pcov = curve_fit(pol1,unp.nominal_values(Frequenz1),unp.nominal_values(B1_ges))
 para_Erdmagnetfeld1 = correlated_values(popt, pcov)
 
-x_fit_1 = np.linspace(5*1e4,5*1e5,2)
+x_fit_1 = np.linspace(5*1e4,1.1*1e6,2)
 y_fit_1 = pol1(x_fit_1,*para_Erdmagnetfeld1)
 
 
@@ -49,7 +49,7 @@ B2_ges = Magnetfeld_Spule(11,P2_V*0.1,0.1639) + Magnetfeld_Spule(154, U_hor2*2, 
 popt, pcov = curve_fit(pol1,unp.nominal_values(Frequenz2),unp.nominal_values(B2_ges))
 para_Erdmagnetfeld2 = correlated_values(popt, pcov)
 
-x_fit_2 = np.linspace(5*1e4,5*1e5,2)
+x_fit_2 = np.linspace(5*1e4,1.1*1e6,2)
 y_fit_2 = pol1(x_fit_2,*para_Erdmagnetfeld2)
 
 print("para_Erdmagnetfeld1: ", para_Erdmagnetfeld1)
@@ -64,7 +64,7 @@ plt.errorbar(unp.nominal_values(Frequenz1)/1e6,unp.nominal_values(B1_ges)*1e6,xe
 
 
 plt.plot(x_fit_2/1e6, unp.nominal_values(y_fit_2)*1e6, c = 'firebrick', label = 'lin. Reg. 2. Peak')
-plt.errorbar(unp.nominal_values(Frequenz2)/1e6,unp.nominal_values(B2_ges)*1e6,xerr=0, yerr=unp.std_devs(B2_ges),color = 'forestgreen', fmt='x',label="2. Peak")
+plt.errorbar(unp.nominal_values(Frequenz2)/1e6,unp.nominal_values(B2_ges)*1e6,xerr=0, yerr=unp.std_devs(B2_ges),color = 'darkolivegreen', fmt='x',label="2. Peak")
 
 
 plt.ylabel(r"$ B \mathbin{/} \unit{\micro\tesla} $")
@@ -74,3 +74,20 @@ plt.tight_layout()
 plt.legend()
 plt.savefig('build/Erdmagnetfeld.pdf')
 plt.clf()
+
+
+gF1 = constants.Planck/(9.2740100783e-24*para_Erdmagnetfeld1[0])
+gF2 = constants.Planck/(9.2740100783e-24*para_Erdmagnetfeld2[0])
+print("gF1: ", gF1)
+print("gF2: ", gF2)
+
+KernS1 = 1/gF1 -1/2
+KernS2 = 1/gF2 -1/2
+print("KernS1: ", KernS1)
+print("KernS2: ", KernS2)
+
+def brechneQuadZee(gf, B, mf, DeltaHyper):
+    return (gf*9.2740100783e-24*B) + (gf*9.2740100783e-24*B)**2*((1-2*mf)/DeltaHyper)
+
+print("QuadZee 1:", brechneQuadZee(gF1, B1_ges[-1], 2, 4.53*1e-24 ))
+print("QuadZee 2:", brechneQuadZee(gF2, B2_ges[-1], 3, 2.01*1e-24 ))
