0777026ac26d7252b6d6154697b0347a22e0671a
Lukas Annuss <lukas.annuss@tu-dortmund.de>
2023-07-04 16:39:11 +0200

    .

--- Praktikum_3/V48/V48/plot.py
+++ Praktikum_3/V48/V48/plot.py
@@ -12,20 +12,21 @@ def methode_eins(T,c,W):
 
 Zeit, Temperatur, Strom, b = np.genfromtxt('Messdaten/Not_my_data.txt',encoding='unicode-escape',unpack=True)
 del b
+Temperatur = Temperatur+constants.zero_Celsius
 Strom = Strom*(1e-12)
 Daten_länge = len(Zeit)
 b = np.zeros(Daten_länge)
 for i in range(1,Daten_länge):
     b[i] = Temperatur[i]-Temperatur[i-1]
 
-Temperatur = Temperatur+constants.zero_Celsius
+
 popt_m_1, pcov_m_1 = curve_fit(methode_eins,Temperatur[Daten_länge-5:],np.log(Strom[Daten_länge-5:]))
 para_m_1 = correlated_values(popt_m_1, pcov_m_1)
 x_fit = np.linspace(-80,80,1000)
 y_fit = methode_eins(x_fit,*para_m_1)
 
 
-plt.scatter(Temperatur,Strom*(1e12),marker='x',s=3,label='Messdaten')
+plt.scatter(Temperatur,np.log(Strom*(1e12)),marker='x',s=3,label='Messdaten')
 #plt.plot(unp.nominal_values(x_fit),unp.nominal_values(y_fit),color='red',label='Fit')
 plt.xlabel(r"$ T  \mathbin{/}  \unit{\celsius} $")
 plt.ylabel(r"$I \mathbin{/} \unit{\pico\ampere} $")
