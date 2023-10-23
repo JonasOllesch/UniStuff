9da089e9dc0ed90a3cb3639bcad276791f5cfe2b
Lukas Annuss <lukas.annuss@tu-dortmund.de>
2023-10-23 09:17:42 +0200

    :

--- Praktikum_4/V64/V64/plot.py
+++ Praktikum_4/V64/V64/plot.py
@@ -8,6 +8,7 @@ import uncertainties.unumpy as unp
 
 from multiprocessing  import Process
 
+
 def berechne_Kontrast(Imax,Imin):
     return (Imax-Imin)/(Imax+Imin)
     
@@ -23,39 +24,48 @@ Messreihe_Glas  = np.genfromtxt('Messdaten/Glas.txt', encoding='unicode-escape')
 Messreihe_Pol   = np.genfromtxt('Messdaten/Pol.txt', encoding='unicode-escape')
 Messreihe_Pol[:,0] = Messreihe_Pol[:,0]*(2*np.pi)/180
 
-Pol_max_mean = np.zeros_like(Messreihe_Pol[:,0])
-Pol_max_std = np.zeros_like(Messreihe_Pol[:,0])
 
-Pol_min_mean = np.zeros_like(Messreihe_Pol[:,0])
-Pol_min_std = np.zeros_like(Messreihe_Pol[:,0])
-
-for i in range(0,len(Messreihe_Pol[:,0])):
-    Pol_max_mean[i] = np.mean(Messreihe_Pol[i,1:4])
-    Pol_max_std[i] = np.std(Messreihe_Pol[i,1:4])
-
-    Pol_min_mean[i] = np.mean(Messreihe_Pol[i,4:7])
-    Pol_min_std[i] = np.std(Messreihe_Pol[i,4:7])
-
-Pol_max = unp.uarray(Pol_max_mean,Pol_max_std)
-Pol_min = unp.uarray(Pol_min_mean,Pol_min_std)
-
-Kontrast = berechne_Kontrast(Pol_max,Pol_min)
+Kontraste = np.zeros((len(Messreihe_Pol[:,0]),3))
+for i in range(0,3):
+    Kontraste[:,i] = berechne_Kontrast(Messreihe_Pol[:,i+1],Messreihe_Pol[:,i+3])
 
 
-popt, pcov = curve_fit(pol_fit, xdata=Messreihe_Pol[:,0],ydata=unp.nominal_values(Kontrast),sigma =unp.std_devs(Kontrast), absolute_sigma=True)
-para_pol = correlated_value(popt, pcov)
-s(popt,pcov)
+#Pol_max_mean = np.zeros_like(Messreihe_Pol[:,0])
+#Pol_max_std = np.zeros_like(Messreihe_Pol[:,0])
+#
+#Pol_min_mean = np.zeros_like(Messreihe_Pol[:,0])
+#Pol_min_std = np.zeros_like(Messreihe_Pol[:,0])
+#
+#for i in range(0,len(Messreihe_Pol[:,0])):
+#    Pol_max_mean[i] = np.mean(Messreihe_Pol[i,1:4])
+#    Pol_max_std[i] = np.std(Messreihe_Pol[i,1:4])
+#
+#    Pol_min_mean[i] = np.mean(Messreihe_Pol[i,4:7])
+#    Pol_min_std[i] = np.std(Messreihe_Pol[i,4:7])
+#
+#Pol_max = unp.uarray(Pol_max_mean,Pol_max_std)
+#Pol_min = unp.uarray(Pol_min_mean,Pol_min_std)
+#
+#Kontrast = berechne_Kontrast(Pol_max,Pol_min)
+#
+#
+#popt, pcov = curve_fit(pol_fit, xdata=Messreihe_Pol[:,0],ydata=unp.nominal_values(Kontrast),sigma =unp.std_devs(Kontrast), absolute_sigma=True)
+#para_pol = correlated_value(popt, pcov)
+#s(popt,pcov)
 
 
 
-def plote_Kontrast(Winkel, Kontrast, para_pol):
+def plote_Kontrast(Winkel, Kontraste):
     x = np.linspace(0,2*np*pi,20000)
     y = np.absolute(np.sin(x))
 
 
-    plt.errorbar(Winkel,unp.nominal_values(Kontrast),yerr=unp.std_devs(Kontrast), fmt ='x', color='darkorange', label='Data')
+    plt.errorbar(Winkel,unp.nominal_values(Kontraste[:,0]),yerr=unp.std_devs(Kontraste[:,0]), fmt ='x', color='darkorange', label='1. Data')
+    plt.errorbar(Winkel,unp.nominal_values(Kontraste[:,1]),yerr=unp.std_devs(Kontraste[:,1]), fmt ='x', color='forestgreen', label='1. Data')
+    plt.errorbar(Winkel,unp.nominal_values(Kontraste[:,2]),yerr=unp.std_devs(Kontraste[:,2]), fmt ='x', color='navy', label='1. Data')
+    
     plt.plot(x, y, color='navy', label = 'Theorie')
-    plt.plot(x,pol_fit(x,*para),color='forestgreen',label='Fit')
+    #plt.plot(x,pol_fit(x,*para),color='forestgreen',label='Fit')
     plt.xlabel(r"$ \theta \mathbin{/} \unit{\degree} $")
     plt.ylabel(r"$K$")
     plt.grid(linestyle = ":")
@@ -66,6 +76,6 @@ def plote_Kontrast(Winkel, Kontrast, para_pol):
 
 
 
-p = Process(target=plote_Kontrast, args=(Messreihe_Pol[:,0], Kontrast ,para_pol))
+p = Process(target=plote_Kontrast, args=(Messreihe_Pol[:,0], Kontraste))
 p.start()
 p.join()
