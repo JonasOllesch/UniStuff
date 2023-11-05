b666da5e5017e20245ffb63d594a4923bc4bd9e0
Lukas Annuss <Lukas.annuss@tu-dortmund.de>
2023-11-05 21:26:47 +0100

    Auswertung

--- Praktikum_4/V46/V46/plot.py
+++ Praktikum_4/V46/V46/plot.py
@@ -10,6 +10,30 @@ import scipy.constants as constants
 #from multiprocessing  import Process
 
 
+def berechne_freien_Winkel(dotiert, undotiert):
+    return np.abs(dotiert-undotiert)
+
+
+def linReg1(Wellenlänge, a, b):
+    n = 3.354
+    N = 2.8*10**24
+    B = 405/1000 *1.02
+    return a*Wellenlänge**2*N*B/n+b*N*B/n
+
+def linReg2(Wellenlänge, a, b):
+    n = 3.354
+    N = 1.2*10**24
+    B = 405/1000 *1.02
+    return a*Wellenlänge**2*N*B/n+b*N*B/n
+
+def berechneEffektiveMasse(a):
+    a = ufloat(np.abs(unp.nominal_values(a)), unp.std_devs(a))
+    u = 8 * np.pi * constants.epsilon_0*constants.c**3*a
+    o = constants.e**3
+
+    return unp.sqrt(o/u)
+
+
 Messreihe1 = np.genfromtxt('Messdaten/P1.txt', encoding='unicode-escape')
 Messreihe2 = np.genfromtxt('Messdaten/P2.txt', encoding='unicode-escape')
 Messreihe3 = np.genfromtxt('Messdaten/P3.txt', encoding='unicode-escape')
@@ -43,17 +67,30 @@ Wellenlänge = Messreihe1[:,0]/(10**6)
 d = np.array([1.296, 1.36,   5.11])
 d = d/1000
 N = np.array([2.8*10**18,1.2*10**18, 0])
-N = N*10**3
+N = N*10**6
 
-diffWinkel = np.zeros((len(Wellenlänge),3))
+diffWinkel_nom = np.zeros((len(Wellenlänge),3))
 
 
-diffWinkel[:,0] = 1/(2*d[0]) *(Winkel1[:,1] -Winkel1[:,0])
-diffWinkel[:,1] = 1/(2*d[1]) *(Winkel2[:,1] -Winkel2[:,0])
-diffWinkel[:,2] = 1/(2*d[2]) *(Winkel3[:,1] -Winkel3[:,0])
+diffWinkel_nom[:,0] = 1/(2*d[0]) *(Winkel1[:,1] -Winkel1[:,0])
+diffWinkel_nom[:,1] = 1/(2*d[1]) *(Winkel2[:,1] -Winkel2[:,0])
+diffWinkel_nom[:,2] = 1/(2*d[2]) *(Winkel3[:,1] -Winkel3[:,0])
 
-print(diffWinkel*180/np.pi)
-   
+
+popt1, pcov1 = curve_fit(linReg1, Wellenlänge, diffWinkel_nom[:,0], absolute_sigma=True)
+print(popt1)
+print(pcov1)
+para1 = correlated_values(popt1, pcov1)
+
+popt2, pcov2 = curve_fit(linReg2, Wellenlänge, diffWinkel_nom[:,1], absolute_sigma=True)
+para2 = correlated_values(popt2, pcov2)
+
+
+print(para1)
+print(para2)
+
+print(repr(berechneEffektiveMasse(para1[0])))
+print(repr(berechneEffektiveMasse(para2[0])))
 
 plt.scatter(Magnetfeld[:,0]*1000, Magnetfeld[:,1]*1000,label = 'Flussdichte',  c='navy', marker = 'x', s = 20)
 plt.hlines(405,xmin=85, xmax=112, colors='navy', linestyles='dotted')
@@ -64,4 +101,28 @@ plt.grid(linestyle = ":")
 plt.tight_layout()
 plt.legend(loc='upper left')
 plt.savefig('build/BFeld.pdf')
+plt.clf()
+
+#plt.scatter(Wellenlänge**2, diffWinkel_nom[:,0], label =" 1. Dotieres", c = 'navy', marker='x' s = 20)
+plt.scatter((Wellenlänge**2)*10**12, diffWinkel_nom[:,0], label ="1. Probe", c = 'darkorange', marker='x' ,s = 20)
+plt.scatter((Wellenlänge**2)*10**12, diffWinkel_nom[:,1], label ="2. Probe", c = 'firebrick', marker='x' ,s = 20)
+plt.scatter((Wellenlänge**2)*10**12, diffWinkel_nom[:,2], label ="3. Probe", c = 'navy', marker='x'  ,s = 20)
+
+#print(Wellenlänge)
+x1 = np.linspace(1*10**(-6),2.65*10**(-6),2)
+y1 = linReg1(x1, a=unp.nominal_values(para1[0]), b=unp.nominal_values(para1[1]))
+
+x2 = np.linspace(1*10**(-6),2.65*10**(-6),2)
+y2 = linReg2(x2, a=unp.nominal_values(para2[0]), b=unp.nominal_values(para2[1]))
+
+plt.plot(x1**2*10**12,y1, c = 'darkorange')
+plt.plot(x2**2*10**12,y2, c = 'firebrick')
+
+
+plt.xlabel(r"$ \lambda \mathbin{/} \unit{\micro\meter²} $")
+plt.ylabel(r"$\theta_{\text{nom}} \mathbin{/} \dfrac{1}{\unit{\meter}}$")
+plt.grid(linestyle = ":")
+plt.tight_layout()
+plt.legend(loc='upper right')
+plt.savefig('build/winkelnom.pdf')
 plt.clf()
