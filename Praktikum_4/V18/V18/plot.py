9b772b0e864ea9409c0a7efe229490afd4e61980
Lukas Annuss <Lukas.annuss@tu-dortmund.de>
2024-01-22 17:39:13 +0100

    :

--- Praktikum_4/V18/V18/plot.py
+++ Praktikum_4/V18/V18/plot.py
@@ -10,6 +10,18 @@ from scipy.signal import find_peaks
 
 def linReg(x,a,b):
     return a*x +b
+
+
+def berechne_Raumwinkel(a, r):
+    return 2*np.pi*(1 -a/np.sqrt(a**2 + r**2))
+
+def berechne_Detektoreffizienz(Linieninhalt, Aktivität, Messzeit, Raumwinkel, Emissionswahrscheinlichkeit):
+    return 4*np.pi*Linieninhalt/(Aktivität * Emissionswahrscheinlichkeit * Messzeit * Raumwinkel)
+
+
+def berechne_Detektoreffizienz_Regression(Energie, alpha, beta):
+    return alpha*Energie**beta
+
 #from matplotlib.legend_handler import (HandlerLineCollection,HandlerTuple)
 #from multiprocessing  import Process
 class Messreihe:
@@ -23,6 +35,10 @@ class Messreihe:
         self.LineContentN = None
         self.LineContentU = None
         self.LineContent = None
+        self.PeakEnergie = None
+        self.PeakEffizienz = None
+        self.Zerfallskonstante = None
+        self.Emissionswahrscheinlichkeit = None
 
 #        self.Effektive_Signale = 0
 
@@ -127,17 +143,17 @@ for i in range(0,len(Europium.Peaks)):
 #    print(Bin[UnterGrenzeP :ObereGrenzeP])
 #
 
-    plt.scatter(Bin[UnterGrenzeP :ObereGrenzeP], Europium.Daten[UnterGrenzeP: ObereGrenzeP], label = f"{i}. Peak", c = "midnightblue", marker='x', s = 10)
-    plt.scatter(FitDataX, FitDataY, label = f"{i}. Linfit?", c = "red", marker='x', s = 10)
-    plt.plot(FitDataX, HintergrundF, label='Hintergrund', color='r')
-#    plt.yscale('log')
-    plt.xlabel("Kanal")
-    plt.ylabel("Signale")
-    plt.grid(linestyle = ":")
-    plt.tight_layout()
-    plt.legend()
-    plt.savefig(f'build/Europium_Peak{i}mit_Hintergrund.pdf')
-    plt.clf()
+#    plt.scatter(Bin[UnterGrenzeP :ObereGrenzeP], Europium.Daten[UnterGrenzeP: ObereGrenzeP], label = f"{i}. Peak", c = "midnightblue", marker='x', s = 10)
+#    plt.scatter(FitDataX, FitDataY, label = f"{i}. Linfit?", c = "red", marker='x', s = 10)
+#    plt.plot(FitDataX, HintergrundF, label='Hintergrund', color='r')
+##    plt.yscale('log')
+#    plt.xlabel("Kanal")
+#    plt.ylabel("Signale")
+#    plt.grid(linestyle = ":")
+#    plt.tight_layout()
+#    plt.legend()
+#    plt.savefig(f'build/Europium_Peak{i}mit_Hintergrund.pdf')
+#    plt.clf()
 
     Europium.LineContentN[i] = np.sum(np.abs(Europium.Daten[UnterGrenzeP: ObereGrenzeP]-HintergrundP))
     Europium.LineContentU[i] = np.sum(np.sqrt(np.abs(Europium.Daten[UnterGrenzeP: ObereGrenzeP]-HintergrundP)))
@@ -155,9 +171,74 @@ for i in range(0,len(Europium.Peaks)):
 #    plt.legend()
 #    plt.savefig(f'build/Europium_Peak{i}ohne_Hintergrund.pdf')
 #    plt.clf()
+    
 Europium.LineContent = unp.uarray(Europium.LineContentN , Europium.LineContentU)
 print(f'Europium LineContent {Europium.LineContent}')
 print(f'Summe der Europiums ohne Hintergrund {sum(Europium.Daten)}')
+
+#Lineare Regression, um die Energie der anderen Kanäle zu bestimmen
+Europium.PeakEnergie = np.array([121.7817, 244.6974, 344.2785, 367.7891, 411.1165, 443.965, 778.9045, 867.380, 964.079, 1085.837, 1112.076, 1408.013])
+Europium.Emissionswahrscheinlichkeit = np.array([ufloat(28.41,13), ufloat(7.55,4), ufloat(26.59,12), ufloat(0.862,5), ufloat(2.238,10), ufloat(2.80,2), ufloat(12.97,6), ufloat(4.243,23), ufloat(14.50,6), ufloat(10.13,6), ufloat(13.41,6), ufloat(20.85,8)])/100
+
+
+
+
+popt_EK, pcov_EK = curve_fit(linReg, Europium.Peaks[1:], Europium.PeakEnergie)
+para_EK = correlated_values(popt_EK, pcov_EK)
+print(f'Die Parameter der linearen Regression zwischen Energie und Kanälen {para_EK}')
+
+
+x = np.linspace(0, 8000, 2)
+y = linReg(x, *unp.nominal_values(para_EK))
+
+plt.plot(x, y, label = "Lineare Regression", color = 'darkorange')
+plt.scatter(Europium.Peaks[1:], Europium.PeakEnergie, label ="Energie der Peaks", c = 'firebrick',marker='x', s = 10)
+
+
+plt.xlabel("Kanal")
+plt.ylabel(r'$E \mathbin{/} \unit{\kilo\eV}$')
+plt.grid(linestyle = ":")
+plt.tight_layout()
+plt.legend()
+plt.savefig('build/EnergieKanäle.pdf')
+plt.clf()
+
+del x
+del y
+
+
+#Berechnung der Detektoreffizienz 
+#Berechnung des Raumwinkels
+
+Raumwinkel = berechne_Raumwinkel(0.086, 0.0225) 
+print(f'Der gegebene Raumwinkel {Raumwinkel}')
+
+A_0 = ufloat(4130, 60)
+Europium.Zerfallskonstante = ufloat(1.6244e-9, 0.0019e-9)
+
+A_t = A_0 * unp.exp(-Europium.Zerfallskonstante* 754963200)
+print(f'Die Aktivität am Messtag {repr(A_t)}')
+Europium.PeakEffizienz = berechne_Detektoreffizienz(Linieninhalt=Europium.LineContent[1:], Aktivität=A_t, Messzeit=Europium.Messzeit, Raumwinkel=Raumwinkel, Emissionswahrscheinlichkeit=Europium.Emissionswahrscheinlichkeit)
+print(f'Die Detektoreffizienz an den Peaks in Prozent {Europium.PeakEffizienz*100}')
+
+
+popt_QE, pcov_QE = curve_fit(berechne_Detektoreffizienz_Regression, unp.nominal_values(Europium.PeakEnergie), unp.nominal_values(Europium.PeakEffizienz))
+para_QE = correlated_values(popt_QE, pcov_QE)
+x = np.linspace(50, 1500, 2000)
+y = berechne_Detektoreffizienz_Regression(x, *unp.nominal_values(para_QE))
+
+plt.plot(x,y, label = 'Regression', color = 'darkorange')
+plt.scatter(unp.nominal_values(Europium.PeakEnergie), unp.nominal_values(Europium.PeakEffizienz), label ="Detektoreffizenz", c = 'firebrick', marker='x', s = 8)
+plt.xlabel(r"$E \mathbin{/} \unit{\kilo\eV}")
+plt.ylabel(r"$Q$")
+plt.grid(linestyle = ":")
+plt.tight_layout()
+plt.legend()
+plt.savefig('build/EffizienzKanal.pdf')
+plt.clf()
+
+
+
 
 
 Bin = np.arange(0, len(HCaesium))
