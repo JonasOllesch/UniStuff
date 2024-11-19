fbb451ed2f343fa376e1f49a9a83943c0eb85361
Lukas Tablet <lukas.annusss@tu-dortmund.de>
2024-11-19 12:59:57 +0100

    .

--- FKT/Blatt 6/omega_plots.py
+++ FKT/Blatt 6/omega_plots.py
@@ -9,7 +9,7 @@ def omega(k, coupling_1, coupling_2, mass, a): # assume equal masses
 
 a = 1 # lattice constant
 K = 1
-mass = 10
+mass = 1
 k = np.linspace(0, 4*np.pi/a + 1e-10, 10000) # k values
 
 
@@ -18,13 +18,14 @@ k = np.linspace(0, 4*np.pi/a + 1e-10, 10000) # k values
 coupling_1_1 = K
 coupling_1_2 = K/2
 
-plt.plot(k, omega(k, coupling_1_1, coupling_1_2, mass, a)[0], label=r'$\omega_+$')
-plt.plot(k, omega(k, coupling_1_1, coupling_1_2, mass, a)[1], label=r'$\omega_-$')
-plt.xticks([0, np.pi/a, 2*np.pi/a], ['0', r'$\pi/a$', r'$2\pi/a$'])
-plt.xlabel(r'$k$ in $\pi/a$')
-plt.ylabel(r'$\omega$')
-plt.legend()
-plt.title(r'$\omega(k)$ for $K_1 = K, K_2 = K/2$ with mass $m = 10$')
+plt.plot(k, omega(k, coupling_1_1, coupling_1_2, mass, a)[0], label=r'$\omega_+$', color ='midnightblue')
+plt.plot(k, omega(k, coupling_1_1, coupling_1_2, mass, a)[1], label=r'$\omega_-$', color = 'firebrick')
+plt.xticks([np.pi/a, 2*np.pi/a, 3*np.pi], [r'$-\pi/a$', '0', r'$+\pi/a$'])
+plt.xlabel(r'$k$')
+plt.ylabel(r'$\omega(k)$')
+plt.legend(loc='lower right')
+plt.grid()
+plt.title(r'$\omega(k)$ for $K_1 = K, K_2 = K/2$ with mass $m = 1$')
 plt.savefig('omega_plots_case_1.pdf')
 plt.clf()
 
@@ -34,12 +35,13 @@ plt.clf()
 coupling_2_1 = K
 coupling_2_2 = K
 
-plt.plot(k, omega(k, coupling_2_1, coupling_2_2, mass, a)[0], label=r'$\omega_+$')
-plt.plot(k, omega(k, coupling_2_1, coupling_2_2, mass, a)[1], label=r'$\omega_-$')
-plt.xticks([0, np.pi/a, 2*np.pi/a], ['0', r'$\pi/a$', r'$2\pi/a$'])
-plt.xlabel(r'$k$ in $\pi/a$')
-plt.ylabel(r'$\omega$')
-plt.legend()
-plt.title(r'$\omega(k)$ for $K_1 = K, K_2 = K$ with mass $m = 10$')
+plt.plot(k, omega(k, coupling_2_1, coupling_2_2, mass, a)[0], label=r'$\omega_+$', color ='midnightblue')
+plt.plot(k, omega(k, coupling_2_1, coupling_2_2, mass, a)[1], label=r'$\omega_-$', color = 'firebrick')
+plt.xticks([np.pi/a, 2*np.pi/a, 3*np.pi], [r'$-\pi/a$', '0', r'$+\pi/a$'])
+plt.xlabel(r'$k$')
+plt.ylabel(r'$\omega$(k)')
+plt.legend(loc='lower right')
+plt.grid()
+plt.title(r'$\omega(k)$ for $K_1 = K, K_2 = K$ with mass $m = 1$')
 plt.savefig('omega_plots_case_2.pdf')
 plt.clf()
