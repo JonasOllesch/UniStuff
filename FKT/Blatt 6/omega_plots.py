b50fcb60c4e6c20e74c8e0a6bef67ef06fbc41f1
Jonas Ollesch <jonas.ollesch@tu-dortmund.de>
2024-11-19 12:37:07 +0100

    .

---
+++ FKT/Blatt 6/omega_plots.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def omega(k, coupling_1, coupling_2, mass, a): # assume equal masses
+    root = np.sqrt(coupling_1**2 + coupling_2**2 + 2*coupling_1*coupling_2*np.cos(a*k))
+    omega_plus = np.sqrt((coupling_1 + coupling_2 + root)/mass)
+    omega_minus = np.sqrt((coupling_1 + coupling_2 - root)/mass)
+    return omega_plus, omega_minus
+
+a = 1 # lattice constant
+K = 1
+mass = 10
+k = np.linspace(0, 4*np.pi/a + 1e-10, 10000) # k values
+
+
+### CASE (i)
+
+coupling_1_1 = K
+coupling_1_2 = K/2
+
+plt.plot(k, omega(k, coupling_1_1, coupling_1_2, mass, a)[0], label=r'$\omega_+$')
+plt.plot(k, omega(k, coupling_1_1, coupling_1_2, mass, a)[1], label=r'$\omega_-$')
+plt.xticks([0, np.pi/a, 2*np.pi/a], ['0', r'$\pi/a$', r'$2\pi/a$'])
+plt.xlabel(r'$k$ in $\pi/a$')
+plt.ylabel(r'$\omega$')
+plt.legend()
+plt.title(r'$\omega(k)$ for $K_1 = K, K_2 = K/2$ with mass $m = 10$')
+plt.savefig('omega_plots_case_1.pdf')
+plt.clf()
+
+
+### CASE (ii)
+
+coupling_2_1 = K
+coupling_2_2 = K
+
+plt.plot(k, omega(k, coupling_2_1, coupling_2_2, mass, a)[0], label=r'$\omega_+$')
+plt.plot(k, omega(k, coupling_2_1, coupling_2_2, mass, a)[1], label=r'$\omega_-$')
+plt.xticks([0, np.pi/a, 2*np.pi/a], ['0', r'$\pi/a$', r'$2\pi/a$'])
+plt.xlabel(r'$k$ in $\pi/a$')
+plt.ylabel(r'$\omega$')
+plt.legend()
+plt.title(r'$\omega(k)$ for $K_1 = K, K_2 = K$ with mass $m = 10$')
+plt.savefig('omega_plots_case_2.pdf')
+plt.clf()
